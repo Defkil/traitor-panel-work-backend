ec58d5a4ca18c61291b1e8fb7191ce17d7d09a79
Oliver Grüttner <gruettner@protonmail.com>
2022-11-04 17:22:38 +0100

    feat: added description

--- src/App.ts
+++ src/App.ts
@@ -1,12 +1,19 @@
-import { inject, injectable } from 'inversify'
+import {inject, injectable, multiInject} from 'inversify'
 import 'reflect-metadata'
 import { TYPES } from './inversify.types'
 import { LoggerInterface } from './service/logger/logger.interface'
 import { ServerInterface } from './service/server/server.interface'
+import {UiInterface} from "./shared/interfaces/ui.interface";
 
 export interface AppInterface {
-  start(): void
-  setup(): void
+  /**
+   * run all necessary setups
+   */
+  setup(): Promise<void>
+  /**
+   * Start the application
+   */
+  start(): Promise<void>
 }
 
 /**
@@ -15,17 +22,44 @@ export interface AppInterface {
 @injectable()
 export class App implements AppInterface {
   constructor(
+      /**
+       * logger service
+       */
     @inject(TYPES.Service.Logger) private logger: LoggerInterface,
-    @inject(TYPES.Service.Server) public server: ServerInterface
+      /**
+       * web server service
+       */
+    @inject(TYPES.Service.Server) private server: ServerInterface,
+      /**
+       * all registered ui services
+       */
+    @multiInject(TYPES.Ui) private uis: UiInterface[]
   ) {}
 
-  public   setup():  void {
+  /**
+   * start mounting ui and after that server setup
+   */
+  public async setup(): Promise<void> {
     this.logger.info('setting up app')
-
+    this.mountUi()
     this.server.setup()
   }
 
-  public start(): void {
+  /**
+   * mount all ui services to fastify
+   * @private
+   */
+  private mountUi(): void {
+    this.uis.forEach(ui => {
+      ui.mount(this.server.fastify)
+    })
+  }
+
+  /**
+   * start fastify server
+   */
+  public async start(): Promise<void> {
+    this.logger.info('starting app')
     this.server.start()
   }
 }
