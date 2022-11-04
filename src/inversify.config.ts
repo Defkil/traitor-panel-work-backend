d833d406fb3315199403d3a88999381029bcfa04
Oliver Grüttner <gruettner@protonmail.com>
2022-11-04 18:29:49 +0100

    feat: added login ui

--- src/inversify.config.ts
+++ src/inversify.config.ts
@@ -4,13 +4,20 @@ import { App, AppInterface } from './App'
 import { Dotenv } from './service/config/dotenv/Dotenv'
 import { Pino } from './service/logger/pino/Pino'
 import { Fastify } from './service/server/fastify/Fastify'
+import { UiInterface } from "./shared/interfaces/ui.interface";
+import { LoginAuthUi } from "./core/auth/ui/login.auth.ui";
 
 const container = new Container()
 
-// bind tools
+// entry point
 container.bind<AppInterface>(TYPES.App).to(App).inSingletonScope()
 
+// bind tools
 container.bind(TYPES.Service.Config).to(Dotenv)
 container.bind(TYPES.Service.Logger).to(Pino)
 container.bind(TYPES.Service.Server).to(Fastify).inSingletonScope()
+
+// bind ui
+container.bind<UiInterface>(TYPES.Ui).to(LoginAuthUi)
+
 export { container }
