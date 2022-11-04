70bfe91be9f8d39c2bea95c7fd7d0e08501459d6
Oliver Grüttner <gruettner@protonmail.com>
2022-11-04 16:52:02 +0100

    refactor: initial test code to own function

--- src/main.ts
+++ src/main.ts
@@ -2,23 +2,25 @@ import { container } from './inversify.config'
 import { TYPES } from './inversify.types'
 import { AppInterface } from './App'
 import { ServerInterface } from './service/server/server.interface'
-/*
-const app = fastify();
 
-app.get('/', async (_request, reply) => {
-    reply.send({ hello: 'world' });
+/**
+ * Main entry point for the application
+ */
+async function start() {
+    const app = container.get<AppInterface>(TYPES.App)
+    await app.setup()
+    await app.start()
+}
+
+start().catch((err) => {
+    console.info('Error starting application', err)
+    console.error(err)
+    process.exit(1)
 })
 
-// @ts-ignore
-if (import.meta.env.PROD)
-    app.listen(3000);
-
-*/
-
-const app = container.get<AppInterface>(TYPES.App)
-app.setup()
-app.start()
-
-export const traitorPanelWorkBackend = container.get<ServerInterface>(
+/**
+ * Vite's server export
+ */
+export const traitorPanel = container.get<ServerInterface>(
   TYPES.Service.Server
 ).fastify
